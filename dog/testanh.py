cfa745072de71c1ed06d58acf47a6cb97ccf873d
Manhhh <89498600+Traianthai@users.noreply.github.com>
2022-10-26 19:48:48 +0700

    Delete testanh.py

--- dog/testanh.py
+++
@@ -1,9 +0,0 @@
-import imp
-import cv2
-img = cv2.imread('GamePyThon/dog/enemies/boom/boom.png',10)
-print(img.shape) 
-
-# for i in range(0,5):
-#     cv2.imwrite(f'boom{i}.png',img[0:179,200*i:200*(i+1)])
-
-# cv2.waitKey(0)
